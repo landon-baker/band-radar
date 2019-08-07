4836a447633a54e1106a137c70053d94415adb29
Landon <landon.baker@utexas.edu>
2019-08-07 15:41:46 -0500

    Add welcome

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <form action="http://localhost:8888/login" method="get">
-      <button type="submit">Login with Spotify</button>
-    </form>
+    <div className="loginWrapper">
+      <div className="row">
+        <h3>Welcome to Band Radar!</h3>
+        <h4>Get started by clicking the button below to login with Spotify!</h4>
+        <div className="row">
+          <form action="http://localhost:8888/login" method="get">
+            <button className="btn" type="submit">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
